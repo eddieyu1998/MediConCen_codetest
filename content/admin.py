e7f353ba80091343df9b01095324c64e2db0e504
eddieyu1998 <eddieyu1998@gmail.com>
2019-05-01 21:46:20 +0800

    Added one model and related dataset

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+from django.apps import apps
 
 # Register your models here.
+models = apps.get_app_config('content')
+
+for name, model in models.models.items():
+    admin.site.register(model)
--- content/models.py
+++ content/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class IrisManager(models.Manager):
+	def create_iris(self, sl, sw, pl, pw, c):
+		iris = self.create(sepal_length=sl, sepal_width=sw, petal_length=pl, petal_width=pw, CLASS=c)
+		return iris
+
+class Iris(models.Model):
+	sepal_length = models.DecimalField(max_digits=2, decimal_places=1)
+	sepal_width = models.DecimalField(max_digits=2, decimal_places=1)
+	petal_length = models.DecimalField(max_digits=2, decimal_places=1)
+	petal_width = models.DecimalField(max_digits=2, decimal_places=1)
+	SETOSA = "SE"
+	VERSICOLOUR = "VE"
+	VIRGINICA = "VI"
+	CLASS_CHOICES = (
+		(SETOSA, "SETOSA"), 
+		(VERSICOLOUR, "VERSICOLOUR"), 
+		(VIRGINICA, "VIRGINICA")
+	)
+	CLASS = models.CharField(max_length=11, choices=CLASS_CHOICES)
+	objects = IrisManager()
